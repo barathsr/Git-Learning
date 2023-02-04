08287358e43f7035a26840eb6f735f3590f86958
Jaya Kumar Murugan <jayakumarm725@gmail.com>
2023-02-04 15:41:36 +0530

    [JayaKumar] Add Intern field to jay

--- names.js
+++ names.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 console.log("Barath, Software Engineer");
 console.log("Jayakumar, Software Engineer");
+=======
+console.log("Barath");
+console.log("Jayakumar,Intern");
+>>>>>>> fe8a53c ([JayaKumar] Add Intern field to jay)
 console.log("Balaji");
 console.log("Jagajeet");
 console.log("Robert");
